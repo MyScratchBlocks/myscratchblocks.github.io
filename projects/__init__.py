df079d7b14ddffce2d382eb2303ae09fc088eb19
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-07-20 12:25:32 +0100

    Update __init__.py

--- projects/__init__.py
+++ projects/__init__.py
@@ -1,6 +1,17 @@
-from flask import *
+from flask import Flask, render_template_string, send_from_directory
+import os
 
-def init_projects(app):
-    @app.route('/projects/<int:id>')
-    def projects(id):
-      return render_template('./index.html', id)
+app = Flask(__name__)
+
+@app.route('/projects/<id>')
+def project_page(id):
+    try:
+        with open('index.html', 'r') as f:
+            html_content = f.read()
+        return render_template_string(html_content, id=id)
+    except FileNotFoundError:
+        return "index.html not found", 404
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory(os.path.join(os.getcwd(), 'js'), filename)
